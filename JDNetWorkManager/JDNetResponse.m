927252a5ae6176843528cb2ffd9f4f6cd1125878
姜锦龙 <lyc59621@icloud.com>
2019-08-08 15:37:02 +0800

    feat: 新建JDNetWorkManager.podspec

---
+++ JDNetWorkManager/JDNetResponse.m
@@ -0,0 +1,13 @@
+//
+//  JDNetResponse.m
+//  JDNetWorkManagerDemo
+//
+//  Created by JDragon on 2019/8/8.
+//  Copyright © 2019 JDragon. All rights reserved.
+//
+
+#import "JDNetResponse.h"
+
+@implementation JDNetResponse
+
+@end
